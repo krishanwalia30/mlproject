2b385bb88e76dd1886018ea6f7f54d1b8ca4480e
Krishan Walia <krishanw30@gmail.com>
2023-12-11 13:05:20 +0530

    Prediction Pipeline Created

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
@@ -19,4 +20,12 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data = CustomData
+        data = CustomData(
+            gender= request.form.get('gender'),
+            race_ethnicity= request.form.get('race_ethnicity'),
+            parental_level_of_education= request.form.get('parental_level_of_education'),
+            lunch= request.form.get('lunch'),
+            test_preparation_course= request.form.get('test_preparation_course'),
+            reading_score= float(request.form.get('reading_score')),
+            writing_score= float(request.form.get('writing_score')),
+        )
